c7131a6a6f6d5fbe6be201af1020ed7b4fc6a9a8
azieelll <145580053+azieelll@users.noreply.github.com>
2024-04-02 21:34:32 +0800

    Add files via upload

---
+++ POSTTEST4/src/menuSistem.java
@@ -0,0 +1,30 @@
+    public class menuSistem {
+
+    public void msg() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("=============================");
+        System.out.println("|      SISTEM PENDATAAN     |");
+        System.out.println("|       ROKOK  DI INDO      |");
+        System.out.println("=============================");
+        System.out.println("");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("|   1. Lihat Daftar Rokok   |");
+        System.out.println("|   2. Tambah Rokok         |");
+        System.out.println("|   3. Ubah Rokok           |");
+        System.out.println("|   4. Hapus Rokok          |");
+        System.out.println("|   0. Exit                 |");
+        System.out.println("|___________________________|");
+        System.out.print("Pilihan : ");
+    }
+
+    protected void tambahData() {
+        System.out.println("========================================");
+        System.out.println("||  1. Tambah Rokok                   ||");
+        System.out.println("||  2. Tambah Kualitas                ||");
+        System.out.println("||  3. Tambah Presentasi Penjualan    ||");
+        System.out.println("||  4. Kembali                        ||");
+        System.out.println("========================================");
+        System.out.print("Pilihan : ");
+    }
+}
